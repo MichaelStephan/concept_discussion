f2d0ab176762d3d5748ae0ac9b327c9e1e606504
Michael Stephan <michael.stephan@sap.com>
2016-05-25 00:17:26 +0200

    update

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :main ^:skip-aot ngom-product.core
+  :main ^:skip-aot concept-discussion.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
