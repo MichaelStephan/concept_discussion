a62638bb41f0a3a56d7850f8e80524d6f78bc1fc
Michael Stephan <michael.stephan@sap.com>
2016-06-01 20:19:21 +0200

    update

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :plugins [[lein2-eclipse "2.0.0"]]
+            :dependencies [[org.clojure/clojure "1.8.0"]
                  [slingshot "0.12.2"]]
   :main ^:skip-aot concept-discussion.core
   :target-path "target/%s"
