46e38463acfb40dbb2245c181347302c1fb4967b
Michael Stephan <michael.stephan@sap.com>
2016-06-03 15:38:13 +0200

    update

--- src/concept_discussion/core.clj
+++ src/concept_discussion/core.clj
@@ -1,19 +1,3 @@
 (ns concept-discussion.core
-  (:require [concept-discussion.product :as product]
-            [concept-discussion.subscription-product :as subscription-product]
-            [concept-discussion.rate-plan :as rate-plan]
-            [concept-discussion.shop-media :as shop-media])
   (:gen-class))
 
-(comment
-  (try
-    (let [product-ref (subscription-product/create! {:product {:name "hugo"}
-                                                     :rate-plan-ref (rate-plan/create! {:terms-and-conditions "Lorem ipsum dolor sit amet ..."})
-                                                     :shop-media-ref (shop-media/create! {:thumbnail "http://blabla.png"})})]
-      
-      )
-    (catch Exception e
-      (.printStackTrace e))))
-
-(comment
-  (l/get-links :pabc 1 :rate-plan))
